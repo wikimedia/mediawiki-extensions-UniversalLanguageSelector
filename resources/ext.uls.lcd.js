99336b1e6237355c79729f2345b355c592675879
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-07-18 17:28:07 +0530

    Fix the wrong license header from Ie0480019
    Change-Id: I518f638b7a0729ad364e6547ee777419de4d4b4f

--- resources/ext.uls.lcd.js
+++ resources/ext.uls.lcd.js
@@ -1,13 +1,12 @@
-<?php
 /**
  * <explain briefly what this file is about>.
  *
  * Copyright (C) 2012 Alolita Sharma, Amir Aharoni, Arun Ganesh, Brandon Harris,
- * Niklas Laxström, Pau Giner, Santhosh Thottingal, Siebrand Mazeland and other
+ * Niklas LaxstrÃ¶m, Pau Giner, Santhosh Thottingal, Siebrand Mazeland and other
  * contributors. See CREDITS for a list.
  *
- * UniversalLanguageSelector is dual licensed GPLv2 or later and MIT. You don’t
- * have to do anything special to choose one license or the other and you don’t
+ * UniversalLanguageSelector is dual licensed GPLv2 or later and MIT. You don't
+ * have to do anything special to choose one license or the other and you don't
  * have to notify anyone which license you are using. You are free to use
  * UniversalLanguageSelector in commercial projects as long as the copyright
  * header is left intact. See files GPL-LICENSE and MIT-LICENSE for details.
