73bb7015c86ae46cb6e018b9d6bcb6cbc2dc2caf
pginer <pginer@wikimedia.org>
2012-11-19 20:20:23 +0100

    CSS fixes to match the style modifications of jquery.uls
    The style rearrangements of jquery.uls (to avoid CSSLint warnings)
    requires some fixes on the extension.
    
    Also update the changes from jquery.uls
    
    Change-Id: I607f7778e620a398ce8648c21c594a3505ec0816

--- resources/js/ext.uls.languagesettings.js
+++ resources/js/ext.uls.languagesettings.js
@@ -36,7 +36,7 @@
 		settingsMenu +
 		settingsPanel +
 		'</div>';
-	windowTemplate = '<div style="display: block;" id="language-settings-dialog" class="uls-menu">'
+	windowTemplate = '<div style="display: block;" id="language-settings-dialog" class="uls-menu uls-wide">'
 		+ closeRow
 		+ panelsRow
 		+ '</div>';
