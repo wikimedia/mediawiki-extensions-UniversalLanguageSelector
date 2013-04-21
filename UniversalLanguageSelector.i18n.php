fdb58375bfdf48cdcfd551bb89bd444abaef6d7f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-04-21 23:51:41 +0300

    Remove uls-select-content-language
    Doesn't seem to be used.
    
    Change-Id: I1ed94d165ccdc8d67278e8ad447322a5e637464e

--- UniversalLanguageSelector.i18n.php
+++ UniversalLanguageSelector.i18n.php
@@ -27,7 +27,6 @@ $messages = array();
 $messages['en'] = array(
 	'UniversalLanguageSelector' => 'Universal Language Selector',
 	'uls-desc' => 'Gives the user several ways to select a language and to adjust language settings',
-	'uls-select-content-language' => 'Select language',
 );
 
 /** Message documentation (Message documentation)
