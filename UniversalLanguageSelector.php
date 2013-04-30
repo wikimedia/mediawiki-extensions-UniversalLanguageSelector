bbeeba5ca70d354664eb6494ea0e100fcb3bab59
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-04-30 21:41:20 +0300

    MediaWiki language extension bundle 2013.04
    Change-Id: I5bdee157f3d6032c037a9c7691749aafd911f858

--- UniversalLanguageSelector.php
+++ UniversalLanguageSelector.php
@@ -30,7 +30,7 @@ define( 'ULS_VERSION', '2013-04-27' );
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'UniversalLanguageSelector',
-	'version' => ULS_VERSION,
+	'version' => '[https://www.mediawiki.org/wiki/MLEB MLEB 2013.04]',
 	'author' => array(
 		'Alolita Sharma',
 		'Amir Aharoni',
