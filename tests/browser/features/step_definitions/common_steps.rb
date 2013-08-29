54afab7ffc1fa2969e93136f7406903a4fb9ce3f
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-08-29 13:56:17 +0300

    Assert that the login succeeded for more helpful error messages
    Change-Id: I9f985d31de86fe2a062d10ca3f51fb2d7c727798

--- tests/browser/features/step_definitions/common_steps.rb
+++ tests/browser/features/step_definitions/common_steps.rb
@@ -1,12 +1,15 @@
 Given(/^I am at random page$/) do
-  visit RandomPage
+	visit RandomPage
 end
 
 Given(/^I am logged out$/) do
 end
 
 Given(/^I am logged in$/) do
-  visit(LoginPage).login_with(@mediawiki_username, @mediawiki_password)
+	visit(LoginPage).login_with(@mediawiki_username, @mediawiki_password)
+	# Assert that login worked
+	loggedin = !@browser.execute_script( "return mw.user.isAnon();" )
+	loggedin.should be_true
 end
 
 def language_to_code(language)
