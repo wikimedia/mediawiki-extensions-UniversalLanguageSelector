74e6ac4fc01e85a3dfdb73b3a4384a0f0b3748d9
Kartik Mistry <kartik.mistry@gmail.com>
2013-09-16 18:26:29 +0530

    Updated: Integration tests for off-screen IME selector
    1. Removed sleep 1.0, not needed,
    2. Removed ascii_vector.rb and ascii_mono.rb as we use, using_params
    now.
    
    Change-Id: Ieeb0eb3a58805fcbfdfd0a3349a427d4757472cb

--- tests/browser/features/step_definitions/uls_ime_steps.rb
+++ tests/browser/features/step_definitions/uls_ime_steps.rb
@@ -72,13 +72,11 @@ Then(/^in it there must be an element with Malayalam text$/) do
 end
 
 When(/^I visit page in Vector skin$/) do
-  sleep 1.0;
-  visit(AsciiVector)
+  visit(ULSPage, :using_params => {:extra => "useskin=vector"})
 end
 
 When(/^I visit page in Monobook skin$/) do
-  sleep 1.0;
-  visit(AsciiMono)
+  visit(ULSPage, :using_params => {:extra => "useskin=monobook"})
 end
 
 Then(/^I should see the input method menu is not offscreen$/) do
--- tests/browser/features/support/pages/ascii_vector.rb
+++
@@ -1,9 +0,0 @@
-class AsciiVector
-  include PageObject
-
-  include URL
-  def self.url
-    URL.url('ASCII?useskin=vector')
-  end
-  page_url url
-end
