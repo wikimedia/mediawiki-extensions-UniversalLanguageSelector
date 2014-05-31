9d7465b726370614360c752ef83205426a1216f1
Kartik Mistry <kartik.mistry@gmail.com>
2014-05-31 11:09:01 +0530

    MediaWiki language extension bundle 2014.05

--- UniversalLanguageSelector.php
+++ UniversalLanguageSelector.php
@@ -30,7 +30,7 @@ define( 'ULS_VERSION', '2014-04-28' );
 $GLOBALS['wgExtensionCredits']['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'UniversalLanguageSelector',
-	'version' => ULS_VERSION,
+	'version' => '[https://www.mediawiki.org/wiki/MLEB MLEB 2014.05]',
 	'author' => array(
 		'Alolita Sharma',
 		'Amir Aharoni',
