5b39955df52d8243de9fffd00e2c006bb8952ae0
Željko Filipin <zeljko.filipin@gmail.com>
2014-06-17 15:22:27 +0200

    Made a scenario pending
    It fails because of a known bug.
    
    Paired with: Amir Aharoni
    Bug: 61214
    
    Change-Id: Id46d5c86e58c4e02bc491378b53c9b3f2f7a4141

--- tests/browser/features/step_definitions/ime_steps.rb
+++ tests/browser/features/step_definitions/ime_steps.rb
@@ -32,7 +32,9 @@ Then(/^I should see a list of available input methods$/) do
 end
 
 Then(/^I should see a list of suggested languages$/) do
-  on(IMEPage).input_method_language_list_element.should be_visible
+  pending("Bug 61214") do
+    on(IMEPage).input_method_language_list_element.should be_visible
+  end
 end
 
 When(/^I choose (.+?) as the input language$/) do |language|
