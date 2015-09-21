e623702647a105fe909818e9e18f331e724ad45a
paladox <thomasmulhall410@yahoo.com>
2015-09-21 17:20:16 +0100

    Add grunt-jscs
    Change-Id: I0213d16e4e1dc3819ae0a663b896033ca572a37d

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
 		jshint: {
@@ -13,6 +14,9 @@ module.exports = function ( grunt ) {
 			all: [
 				'*.js'
 			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
 		},
 		banana: {
 			all: 'i18n/'
@@ -25,6 +29,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
