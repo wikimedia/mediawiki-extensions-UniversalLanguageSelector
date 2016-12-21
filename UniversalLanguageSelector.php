eda6eb8e6ce1d39e3467a8bafa5fce0b7fc91ce5
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-12-21 08:29:51 +0100

    Start showing warnings for deprecated PHP entry point
    Change-Id: I0ef9a614048ec139f15353eac554a91500119eca

--- UniversalLanguageSelector.php
+++ UniversalLanguageSelector.php
@@ -22,11 +22,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'UniversalLanguageSelector' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['UniversalLanguageSelector'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for UniversalLanguageSelector extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'Universal Language Selector extension requires MediaWiki 1.25 or later' );
