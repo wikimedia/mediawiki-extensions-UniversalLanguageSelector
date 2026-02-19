681ca5a8024fe471761a7277c9054ec296fa2aed
Abijeet <abijeetpatro@gmail.com>
2026-02-19 16:19:27 +0530

    Update language name search database
    Fix tests
    
    Bug: T410137
    Change-Id: Ib35b6186c0b6fa9ed63b1801738756ce3cc4f933

--- tests/phpunit/LanguageSearchTest.php
+++ tests/phpunit/LanguageSearchTest.php
@@ -94,9 +94,7 @@ class LanguageSearchTest extends PHPUnit\Framework\TestCase {
 			[ 'punja', [
 				// Presence of CLDR extension affects the results
 				'pa' => class_exists( LanguageNames::class ) ? 'punjabi' : 'punjaabi sennii',
-				'pa-guru' => class_exists( LanguageNames::class ) ?
-					'punjabi (gurmukhi script)' :
-					'punjabi (gurmukhi-skrift)',
+				'pa-guru' => 'punjabi (gurmukhi script)',
 				'pnb' => 'punjabi western'
 			]
 			],
@@ -112,7 +110,6 @@ class LanguageSearchTest extends PHPUnit\Framework\TestCase {
 				'nr' => 'tonga — enetepēra ki te tonga',
 				'hax' => 'tonga — haira ki te tonga',
 				'st' => 'tonga — hōto ki te tonga',
-				'es-419' => 'tonga — pāniora amerikana ki te tonga',
 				'slh' => 'tonga — ratūti ki te tonga',
 				'tce' => 'tonga — tatōne ki te tonga',
 				'alt' => 'tonga — ātai ki te tonga',
