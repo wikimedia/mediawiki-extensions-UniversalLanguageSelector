6f6d4bed1a48a09b44794ede91946a46f0da0349
Huei Tan <htan@wikimedia.org>
2026-06-30 13:30:51 +0200

    ULS rewrite: add EmptySearchEntrypoint test spec
    - shows the empty search description
    - shows the config in action button
    
    Bug: T429115
    Change-Id: I5f3c2659ed89c0cffdfc9a9ce49f3a1c4dd2a574

---
+++ tests/jest/entrypoints/EmptySearchEntrypoint.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Vue = require( 'vue' );
+Vue.createMwApp = Vue.createMwApp || Vue.createApp;
+const { shallowMount } = require( '@vue/test-utils' );
+const EmptySearchEntrypoint = require( '../../../resources/ext.uls.rewrite/entrypoints/EmptySearchEntrypoint.vue' );
+const EntrypointActionButton = require( '../../../resources/ext.uls.rewrite/entrypoints/EntrypointActionButton.vue' );
+
+describe( 'EmptySearchEntrypoint', () => {
+	const config = { label: 'Action 1', url: 'https://example.org' };
+	const mockEntrypoint = {
+		shouldShow: () => true,
+		getConfig: () => config
+	};
+
+	it( 'renders the description paragraph and action buttons', () => {
+		const wrapper = shallowMount( EmptySearchEntrypoint, {
+			global: {
+				mocks: {
+					$i18n: ( key ) => ( {
+						text: () => key,
+						toString: () => key
+					} )
+				}
+			},
+			props: {
+				entrypoints: [ mockEntrypoint ]
+			}
+		} );
+
+		// Verify description is rendered
+		expect( wrapper.find( 'p' ).text() ).toBe( 'ext-uls-empty-state-entrypoint-description' );
+
+		// Verify EntrypointActionButton components are rendered
+		const actionButtons = wrapper.findAllComponents( EntrypointActionButton );
+		expect( actionButtons.length ).toBe( 1 );
+		expect( actionButtons.at( 0 ).props( 'action' ) ).toEqual( config );
+	} );
+
+	it( 'does not render action buttons when shouldShow is false', () => {
+		const mockEntrypointHidden = {
+			shouldShow: () => false,
+			getConfig: () => config
+		};
+		const wrapper = shallowMount( EmptySearchEntrypoint, {
+			global: {
+				mocks: {
+					$i18n: ( key ) => ( {
+						text: () => key,
+						toString: () => key
+					} )
+				}
+			},
+			props: {
+				entrypoints: [ mockEntrypointHidden ]
+			}
+		} );
+
+		const actionButtons = wrapper.findAllComponents( EntrypointActionButton );
+		expect( actionButtons.length ).toBe( 0 );
+	} );
+} );
