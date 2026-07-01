daac63b275929954f1e17e5067187d8d20715ab9
Huei Tan <htan@wikimedia.org>
2026-07-01 11:38:42 +0200

    ULS rewrite: add useEntrypointActions test spec
    - shouldShow and getConfig
    - null configs and flatten array config
    
    Bug: T429115
    Change-Id: I0fafc1ccc2da7a19309d6d6a1a70280fa734e9fc

---
+++ tests/jest/composables/useEntrypointActions.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { ref } = require( 'vue' );
+const useEntrypointActions = require( '../../../resources/ext.uls.rewrite/composables/useEntrypointActions.js' );
+
+describe( 'useEntrypointActions', () => {
+	it( 'shouldShow(context) is called and getConfig(context) is collected', () => {
+		const context = ref( { show: true, value: 'A' } );
+
+		const entrypoint1 = {
+			shouldShow: ( ctx ) => ctx.show,
+			getConfig: ( ctx ) => ( { label: ctx.value } )
+		};
+
+		const entrypoint2 = {
+			shouldShow: () => false,
+			getConfig: () => ( { label: 'B' } )
+		};
+
+		// Basic filtering and resolved config
+		const actions = useEntrypointActions( [ entrypoint1, entrypoint2 ], context );
+		expect( actions.value ).toEqual( [ { label: 'A' } ] );
+
+		// Reactive context update propagation
+		context.value = { show: true, value: 'C' };
+		expect( actions.value ).toEqual( [ { label: 'C' } ] );
+
+		// shouldShow evaluates to false
+		context.value = { show: false, value: 'C' };
+		expect( actions.value ).toEqual( [] );
+	} );
+
+	it( 'handles null configs and flattens array configs', () => {
+		const context = ref( {} );
+
+		const entrypointNull = {
+			shouldShow: () => true,
+			getConfig: () => null
+		};
+
+		const entrypointArray = {
+			shouldShow: () => true,
+			getConfig: () => [ { label: 'Item 1' }, { label: 'Item 2' } ]
+		};
+
+		const entrypointEmptyArray = {
+			shouldShow: () => true,
+			getConfig: () => []
+		};
+
+		const actions = useEntrypointActions(
+			[ entrypointNull, entrypointArray, entrypointEmptyArray ],
+			context
+		);
+
+		expect( actions.value ).toEqual( [
+			{ label: 'Item 1' },
+			{ label: 'Item 2' }
+		] );
+	} );
+} );
