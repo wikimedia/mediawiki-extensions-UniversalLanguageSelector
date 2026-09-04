28f0f47a4e5e5024f23e1aa066b478f1ed336921
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2026-09-04 19:25:27 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.31.0 → 0.32.4
    * grunt: 1.6.2 → 1.6.3
    * svgo: 3.3.3 → 3.3.5
    * brace-expansion: 1.1.13, 2.1.4 → 1.1.18, 2.1.4
      * https://github.com/advisories/GHSA-3jxr-9vmj-r5cp
      * https://github.com/advisories/GHSA-mh99-v99m-4gvg
      * https://github.com/advisories/GHSA-rgw5-rvv9-x895
    * fast-uri: 3.1.2 → 3.1.7
      * https://github.com/advisories/GHSA-4c8g-83qw-93j6
      * https://github.com/advisories/GHSA-7p8r-x3mc-p8w7
      * https://github.com/advisories/GHSA-f65p-4m7j-42xc
      * https://github.com/advisories/GHSA-fph4-wmhf-6fwf
      * https://github.com/advisories/GHSA-jqff-g426-hqxp
      * https://github.com/advisories/GHSA-v2hh-gcrm-f6hx
    * nanoid: 3.3.12 → 3.3.18
      * https://github.com/advisories/GHSA-28wg-ghj8-5hjv
      * https://github.com/advisories/GHSA-2v37-7h3g-55p8
    * postcss: 8.5.13 → 8.5.28
      * https://github.com/advisories/GHSA-fxqj-rqcc-2cmp
      * https://github.com/advisories/GHSA-r28c-9q8g-f849
    
    Additional changes:
    * Disabled Composer audit.block-insecure option (T416518)
    
    Change-Id: If686cc148794e7f5a105b8e5bce126d6422a9ffc

--- resources/js/ext.uls.interface.js
+++ resources/js/ext.uls.interface.js
@@ -123,7 +123,6 @@
 			'cdx-button--weight-quiet',
 			buttonClass
 		];
-		// eslint-disable-next-line mediawiki/class-doc
 		return $( '<button>' ).addClass( classes );
 	}
 
@@ -288,7 +287,6 @@
 				}
 
 				// Message keys listed above
-				// eslint-disable-next-line mediawiki/msg-doc
 				return $( '<p>' ).append( mw.message( messageKey, $link ).parseDom() );
 			}() )
 		} );
